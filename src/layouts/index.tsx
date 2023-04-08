5abc4631ad8c25cdee90a3b52ce2ede4e3fab57b
seongminn <berry9901@gmail.com>
2023-04-08 20:51:56 +0900

    feat: Header 컴포넌트 추가

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -2,6 +2,7 @@ import { ThemeProvider } from '@emotion/react';
 import { ReactNode } from 'react';
 
 import Footer from '@/components/common/Footer';
+import Header from '@/components/common/Header';
 import GlobalStyle from '@/styles/GlobalStyle';
 import theme from '@/styles/theme';
 
@@ -13,6 +14,7 @@ function Layout({ children }: LayoutProps) {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
+      <Header />
       <main>{children}</main>
       <Footer />
     </ThemeProvider>
