7bee6210d991c88b5965353ac98013a646d86fe0
seongminn <berry9901@gmail.com>
2023-04-08 13:57:45 +0900

    chore: meta data를 config 파일 밖에서 별도로 관리

---
+++ gatsby-meta-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  title: `Gatsby Default Starter`,
+  description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+  author: `@gatsbyjs`,
+  siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+  thumbnail: `https://tech.kakaopay.com/_astro/pc_main_bg.bd167a94_Z13qIH1.png`,
+};
