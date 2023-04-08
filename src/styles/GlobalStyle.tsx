0fe4fb2b5459b29f4c738aa4e0e7819b27e7009d
seongminn <berry9901@gmail.com>
2023-04-08 13:56:05 +0900

    style: 전역 스타일 수정

--- src/styles/GlobalStyle.tsx
+++ src/styles/GlobalStyle.tsx
@@ -2,24 +2,44 @@ import { Global, css } from '@emotion/react';
 import { FunctionComponent } from 'react';
 
 const defaultStyle = css`
-  * {
-    padding: 0;
-    margin: 0;
-    box-sizing: border-box;
-    font-family: 'Pretendard';
-  }
-
   html,
   body,
   #___gatsby {
     height: 100%;
   }
 
-  a,
-  a:hover {
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font: inherit;
     color: inherit;
+    flex-shrink: 0;
+  }
+
+  body {
+    font-family: 'Pretendard';
+    letter-spacing: -0.03px;
+  }
+
+  a {
     text-decoration: none;
+    color: inherit;
+  }
+
+  ol,
+  ul {
+    list-style: none;
+  }
+
+  li {
+    list-style: none;
+  }
+
+  button {
     cursor: pointer;
+    border: 0;
+    background-color: inherit;
   }
 `;
 
