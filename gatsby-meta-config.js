39c1f4fb01823654e4ea8eec47e3460812d8e726
seongminn <berry9901@gmail.com>
2023-04-08 22:45:34 +0900

    chore: meta data 수정

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  title: `@seongminn`,
-  description: `돌멩이도 개발할 수 있다.`,
+  title: `seongminn.dev`,
+  description: `기록을 통해 학습한 내용이 의미를 갖도록 하는 공간입니다.`,
   author: `@seongminn`,
   siteUrl: `https://seongminn.netlify.app/`,
   thumbnail: `https://tech.kakaopay.com/_astro/pc_main_bg.bd167a94_Z13qIH1.png`,
