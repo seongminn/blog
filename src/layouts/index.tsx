e8517101db120cc4d82ed5c63cf0be3be6b3f374
seongminn <berry9901@gmail.com>
2023-04-08 13:59:30 +0900

    feat: 공통 layout 컴포넌트 생성

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,23 +1,56 @@
 import { ThemeProvider } from '@emotion/react';
-import styled from '@emotion/styled';
+import { IGatsbyImageData } from 'gatsby-plugin-image';
+import { ReactNode } from 'react';
 
-import Footer from '@/components/common/Footer';
+import Body from './Body';
+import Footer from './Footer';
+import Header from './Header';
+
+import SEO from '@/components/common/SEO';
 import GlobalStyle from '@/styles/GlobalStyle';
 import theme from '@/styles/theme';
 
-function Layout({ children }: { children: React.ReactNode }) {
+type LayoutProps = {
+  title: string;
+  description: string;
+  imageUrl: string;
+  siteUrl: string;
+  imageData: IGatsbyImageData;
+  date?: string;
+  categories?: string[];
+  children: ReactNode;
+};
+
+function Layout({
+  title,
+  description,
+  imageUrl,
+  siteUrl,
+  imageData,
+  date,
+  categories,
+  children,
+}: LayoutProps) {
   return (
     <ThemeProvider theme={theme}>
+      <SEO
+        title={title}
+        description={description}
+        image={imageUrl}
+        url={siteUrl}
+      />
+      <Header
+        title={title}
+        description={description}
+        imageData={imageData}
+        date={date}
+        categories={categories}
+      />
       <GlobalStyle />
-      <BodyWrapper>{children}</BodyWrapper>
+      <Body>{children}</Body>
       <Footer />
     </ThemeProvider>
   );
 }
 
 export default Layout;
-
-const BodyWrapper = styled.main`
-  margin: 0 auto;
-  max-width: 900px;
-`;
