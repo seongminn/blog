c20e0076ea6b3b6934298742f8f73e26efd943d0
seongminn <berry9901@gmail.com>
2023-04-11 03:00:03 +0900

    feat: 돋보기 아이콘 추가

--- src/components/common/Icon/Icons/index.tsx
+++ src/components/common/Icon/Icons/index.tsx
@@ -1,7 +1,8 @@
 import Github from './Github';
 import Google from './Google';
+import Magnifier from './Magnifier';
 
-const iconName = { google: Google, github: Github };
+const iconName = { google: Google, github: Github, magnifier: Magnifier };
 
 export type IconNames = keyof typeof iconName;
 
