5c9e3a151242aa8ca14231c908c7590183a968eb
seongminn <berry9901@gmail.com>
2023-05-04 01:30:29 +0900

    feat: rss 추가 및 github 아이콘 크기 변경

--- src/components/common/Icon/Icons/index.tsx
+++ src/components/common/Icon/Icons/index.tsx
@@ -1,11 +1,13 @@
 import Github from './Github';
 import Menu from './Menu';
+import Rss from './Rss';
 import Search from './Search';
 
 const iconName = {
   github: Github,
   search: Search,
   menu: Menu,
+  rss: Rss,
 };
 
 export type IconNames = keyof typeof iconName;
