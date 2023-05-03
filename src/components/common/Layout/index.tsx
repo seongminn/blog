b382aeaaa85d58a1ab9aeb0503ba24280af9ad9d
seongminn <berry9901@gmail.com>
2023-05-04 01:29:57 +0900

    style: Layout 컴포넌트의 구조 및 스타일 수정

--- src/components/common/Layout/index.tsx
+++ src/components/common/Layout/index.tsx
@@ -1,4 +1,5 @@
 import { ThemeProvider } from '@emotion/react';
+import styled from '@emotion/styled';
 import { ReactNode } from 'react';
 
 import Footer from '@/components/common/Footer';
@@ -13,12 +14,32 @@ type LayoutProps = {
 function Layout({ children }: LayoutProps) {
   return (
     <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <Header />
-      <main>{children}</main>
-      <Footer />
+      <Styled.Root>
+        <Header />
+        <GlobalStyle />
+        <Styled.Container>{children}</Styled.Container>
+        <Footer />
+      </Styled.Root>
     </ThemeProvider>
   );
 }
 
 export default Layout;
+
+const Styled = {
+  Root: styled.div`
+    width: 100%;
+  `,
+  Container: styled.main`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-between;
+
+    padding-top: 84px;
+    margin: 0 auto;
+
+    max-width: ${({ theme }) => theme.layouts.maxWidth};
+    min-width: 300px;
+  `,
+};
